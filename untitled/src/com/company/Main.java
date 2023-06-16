13141941aaec659dead7de90118507f39ac98e74
irinkain <irina.inashvili.1@btu.edu.ge>
2023-06-16 20:38:46 +0400

    java oop1

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -1,17 +1,33 @@
 package com.company;
 
 import com.company.Models.Car;
+import com.company.Models.Circle;
 import com.company.Utils.HelperFunction;
 
 public class Main {
 
     public static void main(String[] args) {
-        Car.setCountry("Germany");
-        Car mercedes = new Car("Mercedes");
-        mercedes.setBrand("mercedes");
-        Car audi = new Car("Audi");
-//        Car opel = new Car();
+//        Car.setCountry("Germany");
+//        Car mercedes = new Car("Mercedes");
+//        mercedes.setBrand("mercedes");
+//        Car audi = new Car("Audi");
+////        Car opel = new Car();
+//
+//        System.out.println(HelperFunction.carToString(audi));
 
-        System.out.println(HelperFunction.carToString(audi));
+
+
+        Circle c1 = new Circle(1,2,3,4);
+        Circle c2 = new Circle();
+        Circle c3 = new Circle(2,3,4);
+        Circle.ricxvi = 10;
+        System.out.println(Circle.ricxvi);
+        c1.setRadius(2.5);
+        c2.setRadius(5);
+        c1.setRadius(3);
+        System.out.println(c1.getLength());
+        System.out.println(c1.getRadius());
+        System.out.println(c2.getDoubleRadius());
+        System.out.println(Circle.MAX_LENGTH);
     }
 }
--- untitled/src/com/company/Models/Circle.java
+++ untitled/src/com/company/Models/Circle.java
@@ -2,10 +2,54 @@ package com.company.Models;
 
 public class Circle {
     private double radius;
-    public void setRadius(double newRadius) {
-        radius = newRadius;
+    private double sigrdze;
+    private double simagle;
+    private double sigane;
+    public static final int MAX_LENGTH = 100;
+    public static final int MAX_LENGTH2 = 100;
+    public static final String UserName = "Nia";
+
+
+    public Circle(double radius, double sigane, double sigrdze, double simagle) {
+        this.radius = radius;
+        this.simagle = simagle;
+        this.sigane = sigane;
+        this.sigrdze = sigrdze;
     }
+
+    public Circle(double radius, double sigane, double sigrdze) {
+        this.radius = radius;
+        this.sigane = sigane;
+        this.sigrdze = sigrdze;
+    }
+
+    public Circle() {
+
+    }
+
+    public static int ricxvi;
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public int getRicxvi() {
+        return ricxvi;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
     public double getLength() {
         return 2 * Math.PI * radius;
     }
+
+    public double getDoubleRadius() {
+        return 2 * radius;
+    }
+
 }
+
+//public, private, protected, default
